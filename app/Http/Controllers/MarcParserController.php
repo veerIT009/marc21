f258a1382e751bc12a242764bf3d2411a1681cd6
RahulUrkude <rahul.u@veerit.com>
2023-10-31 11:30:38 +0530

    Working on marc21 parsing

--- app/Http/Controllers/MarcParserController.php
+++ app/Http/Controllers/MarcParserController.php
@@ -18,9 +18,9 @@ class MarcParserController extends Controller
             $data['isbn'] = $record->getField('020')->getSubField('a')->getData();
             $data['title'] = $record->getField('245')->getSubField('a')->getData();
             $data['edition'] = $record->getField('250')->getSubField('a')->getData();
-            $data['publisher'] = $record->getField('260')->getSubField('a')->getData();
-            $data['genre'] = $record->getField('665')->getSubField('a')->getData();
-            $data['language'] = $record->getField('546')->getSubField('a')->getData();
+            // $data['publisher'] = $record->getField('260')->getSubField('a')->getData();
+            // $data['genre'] = $record->getField('665')->getSubField('a')->getData();
+            // $data['language'] = $record->getField('546')->getSubField('a')->getData();
             // $data['address'] = $record->getField('270')->getSubField('a');
             $data['description'] = $record->getField('300')->getSubField('a')->getData();
             var_dump($data);
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/xml-parser', [MarcParserController::class, 'Parse']);
+Route::get('/', [MarcParserController::class, 'Parse']);
